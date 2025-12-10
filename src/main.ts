49ea6c5e4797a607c8c9c8fccfae89ae73710f3e
adelson70 <bittencourtadelson1@gmail.com>
2025-12-10 17:45:57 -0300

    adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   dotenv.config();
-  
+
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API do projeto')
+    .setVersion('1.0')
+    .addTag('api')
+    .build(),
+  );
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000)
     .then(() => {
       logger.log(`Servidor iniciado na porta ${process.env.PORT ?? 3000}`);
